a0d85d2028f3809bcf5b0f05503d750678db3571
Furkan Ali Tunç <furkanalitunc42@gmail.com>
2024-09-05 16:13:16 +0300

    3.5.1 Polishing1

--- src/main.rs
+++ src/main.rs
@@ -1,7 +1,8 @@
 //! src/main.rs
 use zero2prod::run;
+
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
     // Bubble up the io::Error if we failed to bind the address // Otherwise call .await on our Server
-    run()?.await
+    run("127.0.0.1:8080")?.await
 }
